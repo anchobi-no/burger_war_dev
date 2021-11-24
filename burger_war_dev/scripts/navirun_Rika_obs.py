90d70b58ffd2fee2679afbba3b9909d70ea4b2d1
shiozaki <shiozaki.ryosuke@jp.panasonic.com>
2021-11-25 00:33:04 +0900

    enemy attack

--- burger_war_dev/scripts/navirun_Rika_obs.py
+++ burger_war_dev/scripts/navirun_Rika_obs.py
@@ -19,11 +19,16 @@ pi = math.pi
 PI = math.pi
 
 import actionlib
+from actionlib import SimpleActionClient, GoalStatus
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib_msgs
 
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry   
+
+from std_msgs.msg import Bool
+from std_msgs.msg import Float64
 
 from Enemy_detector_obs import EnemyDetector
 
@@ -47,6 +52,41 @@ TARGET_TH = (
 )
 WIDTH = 1.2 * (2 **0.5) # [m]
 
+def get_goals(my_color = 'r', rotation="CW"):
+    if my_color == 'b':
+        symbol = -1
+        th     = pi
+    else:
+        symbol = 1
+        th     = 0
+
+    # rotation = 'CW'  # 回転方向を変える @en
+
+    if rotation == 'CW':
+        symbol_2 = -1
+    else:
+        symbol_2 = 1
+    goal_xyyaw = np.array([ 
+            [symbol * -0.8  , symbol * 0     , np.mod(0      + th ,2*pi) ], # (1） 
+            [symbol * -0.8  , symbol * -0.2  ,-np.mod(-pi/4  + th ,2*pi) ], 
+            [symbol * -0.8  , symbol * 0     , np.mod(pi/2   + th ,2*pi) ],
+            [symbol * -0.8  , symbol *  0.2  , np.mod(pi/3   + th ,2*pi) ],
+            [symbol * -0.5  , symbol *  0.2  , np.mod(0      + th ,2*pi) ],
+            [symbol * 0     , symbol * 0.4   , np.mod(pi*4/5 + th ,2*pi) ], # (2)
+            [symbol * 0     , symbol * 0.4   , np.mod(-pi/2  + th ,2*pi) ],
+            [symbol * 0.2   , symbol * 0.4   , np.mod( 0     + th ,2*pi) ],
+            [symbol * 0.2   , symbol * 0.4   ,-np.mod(pi/3   + th ,2*pi) ],
+            [symbol * 0.5   , symbol *   0   , np.mod(pi     + th ,2*pi) ], #（3）
+            [symbol * 0.5   , symbol *   0   , np.mod(-pi/2  + th ,2*pi) ],
+            [symbol * 0.2   , symbol * -0.4  , np.mod( pi    + th ,2*pi) ],
+            [symbol * 0.2   , symbol * -0.4  , np.mod(-pi/3  + th ,2*pi) ],
+            [symbol * 0     , symbol * -0.4  , np.mod( 0     + th ,2*pi) ], # (4)
+            [symbol * 0     , symbol * -0.4  , np.mod(-pi*4/5+ th ,2*pi) ],
+            [symbol * 0     , symbol * -0.4  , np.mod( pi    + th ,2*pi) ],
+            [symbol * -0.5  , symbol * 0     , np.mod( pi    + th ,2*pi) ], # (5) 
+            [symbol * -1.40 , symbol * 0     , np.mod( 0     + th ,2*pi) ]
+        ])     
+    return goal_xyyaw 
 class NaviBot():
     def __init__(self):
 
@@ -72,6 +112,9 @@ class NaviBot():
         self.near_wall_range = 0.2  # [m]
         self.speed = 0.07
 
+        self.goals = get_goals()
+        self.goalcounter = 0
+        self.goalcounter_prev = -1
 
         self.pose_twist = Twist()
         self.pose_twist.linear.x = self.speed; self.pose_twist.linear.y = 0.; self.pose_twist.linear.z = 0.
@@ -80,21 +123,53 @@ class NaviBot():
 
         self.is_near_wall = False
 
+        self.is_second_lap = False
+
         self.is_near_enemy = False
         self.enemy_direction = None
-        self.enemy_dist = None
+        self.enemy_dist = 0
+        self.enemy_rad = 0
         self.near_enemy_twist = Twist()
-        self.near_enemy_twist.linear.x = self.speed; self.near_enemy_twist.linear.y = 0.; self.near_enemy_twist.linear.z = 0.
-        self.near_enemy_twist.angular.x = 0.; self.near_enemy_twist.angular.y = 0.; self.near_enemy_twist.angular.z = 0.
-
+        self.near_enemy_twist.linear.x = self.speed 
+        self.near_enemy_twist.linear.y = 0.
+        self.near_enemy_twist.linear.z = 0.
+        self.near_enemy_twist.angular.x = 0. 
+        self.near_enemy_twist.angular.y = 0.
+        self.near_enemy_twist.angular.z = 0.
         self.is_initialized_pose = True
-
+        
         # lidar scan
         self.scan = []
+        self.odom = Odometry()
+        self.odom_prev = Odometry()
+        self.odom_diff = Odometry()
+        
 
         # publisher
-        self.vel_pub = rospy.Publisher('cmd_vel', Twist,queue_size=1)
+        self.vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+        self.odom_pub = rospy.Publisher('odom', Odometry, queue_size= 1)
+        #self.vel_pub = rospy.Publisher('enemy_info', EnemyInfo, queue_size=1)
         # subscriber
+        self.is_enemy_detecting = False
+        rospy.Subscriber('enemy_detected', Bool, self.save_detected )
+        rospy.Subscriber('enemy_dist', Float64, self.save_detected2 )
+        rospy.Subscriber('enemy_rad', Float64, self.save_detected3 )
+        rospy.Subscriber('odom', Odometry, self.save_detected4 )
+
+    def save_detected(self, data):
+        self.is_enemy_detecting = data.data
+        #print("data_updated")
+    def save_detected2(self, data):
+        self.enemy_dist = data.data
+        #print("data_updated")
+    def save_detected3(self, data):
+        self.enemy_rad = data.data
+        #print("data_updated")
+    def save_detected4(self, data):
+        self.odom = data
+        self.odom.pose.pose.position.x += self.odom_diff.pose.pose.position.x
+        self.odom.pose.pose.position.y += self.odom_diff.pose.pose.position.y
+        #print("data_updated")
 
     def setGoal(self,x,y,yaw):
         self.client.wait_for_server()
@@ -113,29 +188,43 @@ class NaviBot():
         goal.target_pose.pose.orientation.w = q[3]
 
         self.client.send_goal(goal)
-        wait = self.client.wait_for_result()
-        if not wait:
-            rospy.logerr("Action server not available!")
-            rospy.signal_shutdown("Action server not available!")
-        else:
-            return self.client.get_result()        
+        #wait = self.client.wait_for_result()
+        #if not wait:
+        #    rospy.logerr("Action server not available!")
+        #    rospy.signal_shutdown("Action server not available!")
+        #else:
+        #    return self.client.get_result()        
 
+        def active_cb():
+            # rospy.loginfo("active_cb. Goal pose is now being processed by the Action Server...")
+            return
+
+        def feedback_cb( feedback):
+            #To print current pose at each feedback:
+            #rospy.loginfo("Feedback for goal "+str(self.goal_cnt)+": "+str(feedback))
+            # rospy.loginfo("feedback_cb. Feedback for goal pose received{}".format(feedback))
+            return
+
+        def done_cb(status, result):
+            if status is not GoalStatus.PREEMPTED:
+                self.goalcounter += 1
+                if self.goalcounter == len(self.goals):
+                    self.is_second_lap = True
+                self.goalcounter %= len(self.goals)
+
+                # 2周目の場合は、最初の方のマーカーをとばす
+                if self.is_second_lap and (self.goalcounter in [0, 1, 2]):
+                    self.goalcounter = 3
+            #rospy.loginfo("done_cb. status:{} result:{}".format(num2mvstate(status), result))
+
+        self.client.send_goal(goal, done_cb=done_cb, active_cb=active_cb, feedback_cb=feedback_cb)
+        return
 
     def strategy(self):
         r = rospy.Rate(5) # change speed 5fps
         map_name=self.robot_namespace+'/map'
         is_enemy_detected, x, y = self.getEnemyPos(map_name)
         print(is_enemy_detected)
-        #self.setGoal(-0.5,0,0)
-        #self.setGoal(-0.5,0,3.1415/2)
-        
-        #self.setGoal(0,0.5,0)
-        #self.setGoal(0,0.5,3.1415)
-        
-        #self.setGoal(-0.5,0,-3.1415/2)
-        
-        #self.setGoal(0,-0.5,0)
-        #self.setGoal(0,-0.5,3.1415)
         symbol = 1
         th = 0
         goal_xyyaw = np.array([ 
@@ -158,22 +247,75 @@ class NaviBot():
                 [symbol * -0.5  , symbol * 0     , np.mod( pi    + th ,2*pi) ] # (5) 
         ])     
         idx=0
-        while(1):
-            goal_val0 = goal_xyyaw[idx%len(goal_xyyaw)][0]
-            goal_val1 = goal_xyyaw[idx%len(goal_xyyaw)][1]
-            goal_val2 = goal_xyyaw[idx%len(goal_xyyaw)][2]
-            # 敵の検出
-            is_enemy_detected, enemy_dist, enemy_rad = self.getEnemyDistRad()
-            # rospy.loginfo("is_enemy_detected:{} enemy_dist{} enemy_rad:{}".format(is_enemy_detected, enemy_dist, enemy_rad))
 
-            #map_name=self.robot_namespace+'/map'
-            if is_enemy_detected:
-                print(enemy_rad)
-                print(enemy_dist)
+        is_patrol_mode_prev = False
+        is_patrol_mode = True
+        cnt = 0
+        flg = True
+
+
+        while(True):
+            r.sleep()
+            is_patrol_mode = True
+            detect_inner_th = 1.2
+            detect_outer_th = 0.7
+            if not self.is_enemy_detecting:
+                is_patrol_mode = True
+            elif self.is_enemy_detecting and flg and detect_inner_th > self.enemy_dist:
+                is_patrol_mode = False
+            #elif not is_patrol_mode and detect_outer_th < self.enemy_dist:
+            #    is_patrol_mode = True
+            if is_patrol_mode and (not is_patrol_mode_prev or (self.goalcounter is not self.goalcounter_prev)):
+                print("次のゴールに行くぜ〜")
+                # 新たに巡回モードに切り替わった瞬間及びゴール座標が変わった時
+                # goalcounterのゴール座標をセット
+                goal_val0 = goal_xyyaw[idx%len(goal_xyyaw)][0]
+                goal_val1 = goal_xyyaw[idx%len(goal_xyyaw)][1]
+                goal_val2 = goal_xyyaw[idx%len(goal_xyyaw)][2]
+                self.setGoal(goal_val0, goal_val1, goal_val2)
+                #rospy.loginfo( num2mvstate(self.client.get_state()))
+                self.goalcounter_prev = idx%len(goal_xyyaw)
+                idx+=1
+                self.odom_pub.publish(self.odom )
+
+            
+            elif is_patrol_mode:
+                print("巡回するぜ〜")
+                # 巡回モード最中。CBが来るまで何もしない。
+                pass
             else:
-                print("no enemy")
-            self.setGoal(goal_val0, goal_val1, goal_val2)
-            idx += 1
+                print("Yeah!! Enemy Det!!敵を見るぜ〜")
+                #goal_val0 = self.enemy_dist*math.sin(self.enemy_rad + pi)
+                #goal_val1 = self.enemy_dist*math.cos(self.enemy_rad + pi)
+                #goal_val2 = self.enemy_rad + pi
+                #self.setGoal(goal_val0, goal_val1, goal_val2)
+                
+
+                # 敵の方向を向くモード
+                if cnt < 1 :
+                    self.odom_prev = self.odom
+                self.client.cancel_all_goals()
+                if cnt > 50 :
+                    flg = False
+                    self.odom_diff.pose.pose.position.x = self.odom_prev.pose.pose.position.x - self.odom.pose.pose.position.x
+                    self.odom_diff.pose.pose.position.y = self.odom_prev.pose.pose.position.y - self.odom.pose.pose.position.y
+                    continue
+                # import pdb; pdb.set_trace()
+                
+                if self.enemy_dist < 0.5 :
+                    cnt += 1
+                    self.odom_pub.publish(self.odom_prev)
+                # print("POSE TWIST: {}, {}".format(self.pose_twist.linear.x, self.pose_twist.angular.z))
+                # print("ENEMY TWIST: {}, {}".format(self.near_enemy_twist.linear.x, self.near_enemy_twist.angular.z))
+                # print("wall: {}, Enemy: {}, X: {}, Z: {}".format(self.is_near_wall, self.is_near_enemy, twist.linear.x, twist.angular.z))
+                twist = Twist()
+                twist.linear.x = 0.1
+                twist.angular.z = self.enemy_rad
+                self.vel_pub.publish(twist)
+                
+                # pass
+                
+            is_patrol_mode_prev = is_patrol_mode
 
     def getEnemyPos(self, frame):
         try:
